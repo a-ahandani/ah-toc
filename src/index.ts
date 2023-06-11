f29b9bd493afd979731832a78ca2dcfa6120b3b7
Ahmad Ahandani <a.e.ahandani@gmail.com>
2023-06-10 23:21:21 -0700

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import toc from "./table-of-contents";
+import toc, { NodeType as NT, OptionsType as OT } from "./table-of-contents";
 import hook from "./use-table-of-contents";
 
-
+export type NodeType = NT;
+export type OptionsType = OT;
 export const TableOfContents = toc;
 export const useTableOfContents = hook;
